c2a306ac15c404b2085c59524174e47d85d2bd40
0118Add <60157610+0118Add@users.noreply.github.com>
2023-03-03 08:30:01 +0800

    Update ceshi-x86.sh

--- x86/ceshi-x86.sh
+++ x86/ceshi-x86.sh
@@ -74,6 +74,9 @@ rm -rf feeds/luci/applications/luci-app-dockerman
 #rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
 #rm -rf feeds/packages/multimedia/aliyundrive-webdav
 
+#rm -rf package/kernel/mt76
+#git checkout b9408b6b3933d93ca40f62659903bf4636409d75 package/kernel/mt76
+
 # 添加额外软件包
 #git clone https://github.com/0118Add/build-actions.git package/myautocore
 svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
