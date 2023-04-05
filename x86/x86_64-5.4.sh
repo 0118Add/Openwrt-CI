f1a349e4618752321217c6195458be4fbaee50c9
0118Add <60157610+0118Add@users.noreply.github.com>
2023-04-05 11:57:57 +0800

    Update x86_64-5.4.sh

--- x86/x86_64-5.4.sh
+++ x86/x86_64-5.4.sh
@@ -40,7 +40,6 @@ rm -rf feeds/luci/applications/luci-app-passwall
 #rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
-rm -rf feeds/packages/net/frp
 rm -rf feeds/packages/net/zerotier
 rm -rf feeds/luci/applications/luci-app-zerotier
 #git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
@@ -59,7 +58,6 @@ git clone https://github.com/sbwml/luci-app-alist.git package/alist
 #git clone https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-svn co https://github.com/coolsnowwolf/packages/trunk/net/frp package/frp
 svn co https://github.com/0118Add/openwrt-packages/trunk/luci-app-frpc package/luci-app-frpc
 svn co https://github.com/0118Add/openwrt-packages/trunk/zerotier package/zerotier
 svn co https://github.com/0118Add/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
