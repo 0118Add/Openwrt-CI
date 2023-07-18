e48af661689964e09ec9e3b5c54ebb1ec46fb2ee
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-18 14:34:32 +0800

    Update x86_64-5.4.sh

--- x86/x86_64-5.4.sh
+++ x86/x86_64-5.4.sh
@@ -5,7 +5,7 @@
 #sed -i 's/PATCHVER:=4.19/PATCHVER:=4.14/g' ./target/linux/x86/Makefile
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改主机名
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
