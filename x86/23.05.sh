0b7f804c81473fbd0118ec5bbfaaaf87f3784a54
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-18 19:47:58 +0800

    Update 23.05.sh

--- x86/23.05.sh
+++ x86/23.05.sh
@@ -59,6 +59,7 @@ git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.gi
 #svn co https://github.com/0118Add/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
+svn co https://github.com/fw876/helloworld/trunk/tuic-client package/tuic-client
 #svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 #git clone https://github.com/sbwml/luci-app-alist.git package/alist
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
