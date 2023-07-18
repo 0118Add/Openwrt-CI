b43284b09b408ed8f5276e25bc3a69491ec4ba67
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-18 14:34:05 +0800

    Update ceshi-x86.sh

--- x86/ceshi-x86.sh
+++ x86/ceshi-x86.sh
@@ -18,7 +18,7 @@
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
