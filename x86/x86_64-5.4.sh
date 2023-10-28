94384779de7328adbc51dac3f9fb5c9b2c822758
0118Add <60157610+0118Add@users.noreply.github.com>
2023-10-28 12:13:20 +0800

    Update x86_64-5.4.sh

--- x86/x86_64-5.4.sh
+++ x86/x86_64-5.4.sh
@@ -57,8 +57,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 #svn co https://github.com/xiaorouji/openwrt-passwall/branches/packages/sing-box package/sing-box
-#svn co https://github.com/xiaorouji/openwrt-passwall/branches/packages/shadow-tls package/shadow-tls
 #svn co https://github.com/xiaorouji/openwrt-passwall/branches/packages/tuic-client package/tuic-client
+svn co https://github.com/fw876/helloworld/trunk/shadow-tls package/shadow-tls
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 #git clone https://github.com/sbwml/luci-app-alist.git package/alist
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
