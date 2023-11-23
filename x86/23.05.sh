64dd59d4ccdbc0759b3d6608ba4a1c583473b115
0118Add <60157610+0118Add@users.noreply.github.com>
2023-11-23 19:28:20 +0800

    Update 23.05.sh

--- x86/23.05.sh
+++ x86/23.05.sh
@@ -17,6 +17,10 @@
 #sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION=''|g" package/base-files/files/etc/openwrt_release
 #sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='OpenWrt 23.05'|g" package/base-files/files/etc/openwrt_release
 
+# 移除 SNAPSHOT 标签
+sed -i 's,-SNAPSHOT,,g' include/version.mk
+sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
+
 # 修改IP地址
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
