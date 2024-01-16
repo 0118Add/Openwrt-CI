85aeaf39281dbf0e4562ee2bd65daeea102a6a54
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-16 20:35:07 +0800

    Update DHDAXCW-Lede.sh

--- x86/DHDAXCW-Lede.sh
+++ x86/DHDAXCW-Lede.sh
@@ -14,8 +14,8 @@ git clone https://github.com/sbwml/luci-app-alist package/alist
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
-mkdir package/community
-pushd package/community
+#mkdir package/community
+#pushd package/community
 
 # Add Lienol's Packages
 #git clone --depth=1 https://github.com/Lienol/openwrt-package
