e09852b3973d91c42aae89f6cd5cdc36e925f070
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-27 12:36:38 +0800

    Update master.sh

--- x86/master.sh
+++ x86/master.sh
@@ -143,7 +143,8 @@ git clone --depth=1 -b dev https://github.com/vernesong/OpenClash package/OpenCl
 #chmod 0755 package/luci-app-bypass/root/etc/init.d/bypass
 
 # 修改系统文件
-curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/25_storage.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/25_storage.js
+wget -O ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/25_storage.js https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/25_storage.js
+#curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/25_storage.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/25_storage.js
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
