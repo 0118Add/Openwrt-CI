c986e6d45d54f816b9a34a4b0ef93ce4b5032a1a
0118Add <60157610+0118Add@users.noreply.github.com>
2024-09-01 17:30:32 +0800

    Update 23.05.sh

--- x86/23.05.sh
+++ x86/23.05.sh
@@ -79,6 +79,7 @@ git clone -b luci-smartdns-dev --single-branch https://github.com/lwb1978/openwr
 git clone https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
 #git clone --single-branch https://github.com/lwb1978/luci-app-smartdns package/luci-app-smartdns
 #git clone https://github.com/QiuSimons/luci-app-daed-next package/luci-app-daed-next
+git clone -b luci-app-neko --depth 1 https://github.com/Thaolga/neko package/neko
 git clone --depth=1 -b dev https://github.com/vernesong/OpenClash package/OpenClash
 
 # mihomo
