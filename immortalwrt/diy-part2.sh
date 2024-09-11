c9cf71cdaf4e13e2c9129e07be6ccb88b298b6fb
0118Add <60157610+0118Add@users.noreply.github.com>
2024-09-11 13:24:53 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -162,6 +162,8 @@ sed -i '/exit 0$/d' package/emortal/default-settings/files/99-default-settings
 cat ${GITHUB_WORKSPACE}/immortalwrt/default-settings >> package/emortal/default-settings/files/99-default-settings
 rm -rf feeds/packages/lang/ruby
 cp -rf $GITHUB_WORKSPACE/patch/ruby feeds/packages/lang/ruby
+rm -rf package/emortal/automount
+rm -rf feeds/luci/applications/luci-app-polipo
 
 # comment out the following line to restore the full description
 #sed -i '/# timezone/i grep -q '\''/tmp/sysinfo/model'\'' /etc/rc.local || sudo sed -i '\''/exit 0/i [ "$(cat /sys\\/class\\/dmi\\/id\\/sys_vendor 2>\\/dev\\/null)" = "Default string" ] \&\& echo "x86_64" > \\/tmp\\/sysinfo\\/model'\'' /etc/rc.local\n' package/emortal/default-settings/files/99-default-settings
