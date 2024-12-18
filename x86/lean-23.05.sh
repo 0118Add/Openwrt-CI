084aaa1a761ad88c22d325bcd0344c397e031d5d
0118Add <60157610+0118Add@users.noreply.github.com>
2024-12-18 15:42:21 +0800

    Update lean-23.05.sh

--- x86/lean-23.05.sh
+++ x86/lean-23.05.sh
@@ -250,6 +250,7 @@ sed -i 's/vpn/services/g' ./feeds/luci/applications/luci-app-zerotier/root/usr/s
 # 修改系统文件
 #curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/10_system.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/25_storage.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/25_storage.js
+curl -fsSL https://raw.githubusercontent.com/0118Add/Cloudbuild/main/patches/29_ports.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js
 curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/30_network.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/30_network.js
 
 # 修改 Makefile
