088f1a8080f5a8a52026cd30363053268f8bb9ee
0118Add <60157610+0118Add@users.noreply.github.com>
2025-01-14 15:36:47 +0800

    Update lean-23.05.sh

--- x86/lean-23.05.sh
+++ x86/lean-23.05.sh
@@ -129,6 +129,7 @@ rm -rf feeds/luci/applications/luci-app-zerotier
 #merge_package https://github.com/mgz0227/OP-Packages OP-Packages/dae
 git clone -b luci-smartdns-dev --single-branch https://github.com/lwb1978/openwrt-passwall package/passwall-luci
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+rm -rf package/openwrt-passwall/sing-box
 git clone --single-branch https://github.com/lwb1978/luci-app-smartdns package/luci-app-smartdns
 cp -rf ${GITHUB_WORKSPACE}/patch/smartdns feeds/packages/net
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
