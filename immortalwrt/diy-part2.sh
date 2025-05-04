14c5b16f8d62c2850a4502a59fab2138327776d9
0118Add <60157610+0118Add@users.noreply.github.com>
2025-05-04 17:23:28 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -58,7 +58,6 @@ merge_package v5 https://github.com/sbwml/openwrt_helloworld package/passwall-pa
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone --single-branch https://github.com/lwb1978/luci-app-smartdns package/luci-app-smartdns
 # 替换immortalwrt 软件仓库smartdns版本为官方最新版
-rm -rf package/kernel/r8152
 rm -rf feeds/packages/net/smartdns
 cp -rf ${GITHUB_WORKSPACE}/patch/smartdns feeds/packages/net
 
