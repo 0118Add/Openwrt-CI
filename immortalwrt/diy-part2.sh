fe338f30e0eb6a9c50f48eb7164ec9b9aa698e92
0118Add <60157610+0118Add@users.noreply.github.com>
2025-08-06 16:41:00 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -80,13 +80,13 @@ git clone https://github.com/nikkinikki-org/OpenWrt-nikki  package/OpenWrt-nikki
 
 # neko
 #git clone -b nekobox --depth 1 https://github.com/Thaolga/openwrt-nekobox package/nekobox
-#sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
+sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
 
 # partexp
 git clone https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
 
 # tailscale
-git clone https://github.com/Jaykwok2999/luci-app-tailscale  package/luci-app-tailscale
+#git clone https://github.com/Jaykwok2999/luci-app-tailscale  package/luci-app-tailscale
 
 # golang 1.25
 rm -rf feeds/packages/lang/golang
