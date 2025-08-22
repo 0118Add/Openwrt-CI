f78342a0b26cd9354e563004cd3ba49b18216e81
0118Add <60157610+0118Add@users.noreply.github.com>
2025-08-22 22:04:09 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -71,7 +71,7 @@ git clone --depth=1 -b dev https://github.com/vernesong/OpenClash package/OpenCl
 
 # homeproxy
 rm -rf feeds/luci/applications/luci-app-homeproxy
-git clone --depth=1 -b next https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+git clone --depth=1 -b dev https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 sed -i "s/ImmortalWrt/OpenWrt/g" package/luci-app-homeproxy/po/zh_Hans/homeproxy.po
 sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
 
