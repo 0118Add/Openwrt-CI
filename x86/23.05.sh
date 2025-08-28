74e2021bf0130eedb300cfd0a0aba061b9178517
0118Add <60157610+0118Add@users.noreply.github.com>
2025-08-28 10:47:32 +0800

    Update 23.05.sh

--- x86/23.05.sh
+++ x86/23.05.sh
@@ -20,8 +20,8 @@ source ${GITHUB_WORKSPACE}/patch/function.sh
 sed -i "s/hostname='.*'/hostname='ImmortalWrt'/g" package/base-files/files/bin/config_generate
 
 # 更改固件版本信息
-#sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION=''|g" package/base-files/files/etc/openwrt_release
-#sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='OpenWrt %V'|g" package/base-files/files/etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION=''|g" package/base-files/files/etc/openwrt_release
+sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='ImmortalWrt 23.05.5'|g" package/base-files/files/etc/openwrt_release
 
 # 修改x86内核到6.6版
 #sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
