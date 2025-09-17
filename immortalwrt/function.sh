e2100cfc1d80bcc3b4d7fd078a8021f62f79326e
0118Add <60157610+0118Add@users.noreply.github.com>
2025-09-17 16:09:44 +0800

    Update function.sh

--- immortalwrt/function.sh
+++ immortalwrt/function.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-# 打包Toolchain
-if [[ $REBUILD_TOOLCHAIN = 'true' ]]; then
-    echo -e "\e[1;33m开始打包toolchain目录\e[0m"
-    cd $OPENWRT_PATH
-    sed -i 's/ $(tool.*\/stamp-compile)//' Makefile
-    [ -d ".ccache" ] && (ccache=".ccache"; ls -alh .ccache)
-    du -h --max-depth=1 ./staging_dir
-    du -h --max-depth=1 ./ --exclude=staging_dir
-    tar -I zstdmt -cf $GITHUB_WORKSPACE/output/$CACHE_NAME.tzst staging_dir/host* staging_dir/tool* $ccache
-    ls -lh $GITHUB_WORKSPACE/output
-    [ -e $GITHUB_WORKSPACE/output/$CACHE_NAME.tzst ] || exit 1
-    exit 0
-fi
-
-[ -d $GITHUB_WORKSPACE/output ] || mkdir $GITHUB_WORKSPACE/output
-
 # 拉取仓库文件夹
+merge_package() {
+	# 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+	# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+	# 示例:
+	# merge_package master https://github.com/WYC-2020/openwrt-packages package/openwrt-packages luci-app-eqos luci-app-openclash luci-app-ddnsto ddnsto 
+	# merge_package master https://github.com/lisaac/luci-app-dockerman package/lean applications/luci-app-dockerman
+	if [[ $# -lt 3 ]]; then
+		echo "Syntax error: [$#] [$*]" >&2
+		return 1
+	fi
+	trap 'rm -rf "$tmpdir"' EXIT
+	branch="$1" curl="$2" target_dir="$3" && shift 3
+	rootdir="$PWD"
+	localdir="$target_dir"
+	[ -d "$localdir" ] || mkdir -p "$localdir"
+	tmpdir="$(mktemp -d)" || exit 1
+	git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+	cd "$tmpdir"
+	git sparse-checkout init --cone
+	git sparse-checkout set "$@"
+	# 使用循环逐个移动文件夹
+	for folder in "$@"; do
+		mv -f "$folder" "$rootdir/$localdir"
+	done
+	cd "$rootdir"
+}
+
 drop_package(){
 	find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
 }
@@ -26,185 +37,54 @@ merge_feed(){
 	./scripts/feeds install -a -p $1
 }
 
-color() {
-    case $1 in
-        cr) echo -e "\e[1;31m$2\e[0m" ;;
-        cg) echo -e "\e[1;32m$2\e[0m" ;;
-        cy) echo -e "\e[1;33m$2\e[0m" ;;
-        cb) echo -e "\e[1;34m$2\e[0m" ;;
-        cp) echo -e "\e[1;35m$2\e[0m" ;;
-        cc) echo -e "\e[1;36m$2\e[0m" ;;
-        ch) echo -e "\e[1;41m$2\e[0m" ;;
-    esac
+# 版本比较 sh
+check_ver() {
+	local version1="$1"
+	local version2="$2"
+	# 分割版本号字符串并设置默认值
+	local i v1 v1_1 v1_2 v1_3 v2 v2_1 v2_2 v2_3
+	IFS='.'; set -- $version1; v1_1=${1:-0}; v1_2=${2:-0}; v1_3=${3:-0}
+	IFS='.'; set -- $version2; v2_1=${1:-0}; v2_2=${2:-0}; v2_3=${3:-0}
+	IFS=
+	# 逐个比较版本号段
+	for i in 1 2 3; do
+		eval v1=\$v1_$i
+		eval v2=\$v2_$i
+		if [ "$v1" -gt "$v2" ]; then
+			# echo "版本 $version1 大于版本 $version2"
+			echo 0
+			return
+		elif [ "$v1" -lt "$v2" ]; then
+			# echo "版本 $version1 小于版本 $version2"
+			echo 1
+			return
+		fi
+	done
+	# echo "版本 $version1 等于版本 $version2"
+	echo 255
 }
 
-status() {
-    local check=$? end_time=$(date '+%H:%M:%S') total_time
-    total_time="==> 用时 $[$(date +%s -d $end_time) - $(date +%s -d $begin_time)] 秒"
-    [[ $total_time =~ [0-9]+ ]] || total_time=""
-    if [[ $check = 0 ]]; then
-        printf "%-62s %s %s %s %s %s %s %s\n" \
-        $(color cy $1) [ $(color cg ✔) ] $(echo -e "\e[1m$total_time")
-    else
-        printf "%-62s %s %s %s %s %s %s %s\n" \
-        $(color cy $1) [ $(color cr ✕) ] $(echo -e "\e[1m$total_time")
-    fi
+# 版本比较 bash
+check_ver2() {
+	local version1="$1"
+	local version2="$2"
+	local v1={}
+	local v2={}
+	# 将版本号字符串分割成数组
+	IFS='.' read -ra v1 <<< "$version1"
+	IFS='.' read -ra v2 <<< "$version2"
+	# 逐个比较数组中的元素
+	for i in "${!v1[@]}"; do
+		if [ "${v1[i]}" -gt "${v2[i]}" ]; then
+			# echo "版本 $version1 大于版本 $version2"
+			echo 0
+			return
+		elif [ "${v1[i]}" -lt "${v2[i]}" ]; then
+			# echo "版本 $version1 小于版本 $version2"
+			echo 1
+			return
+		fi
+	done
+	# echo "版本 $version1 等于版本 $version2"
+	echo 255
 }
-
-find_dir() {
-    find $1 -maxdepth 3 -type d -name $2 -print -quit 2>/dev/null
-}
-
-print_info() {
-    printf "%s %-40s %s %s %s\n" $1 $2 $3 $4 $5
-    # read -r param1 param2 param3 param4 param5 <<< $1
-    # printf "%s %-40s %s %s %s\n" $param1 $param2 $param3 $param4 $param5
-}
-
-# 添加整个源仓库(git clone)
-git_clone() {
-    local repo_url branch target_dir current_dir
-    if [[ "$1" == */* ]]; then
-        repo_url="$1"
-        shift
-    else
-        branch="-b $1 --single-branch"
-        repo_url="$2"
-        shift 2
-    fi
-    if [[ -n "$@" ]]; then
-        target_dir="$@"
-    else
-        target_dir="${repo_url##*/}"
-    fi
-    git clone -q $branch --depth=1 $repo_url $target_dir 2>/dev/null || {
-        print_info $(color cr 拉取) $repo_url [ $(color cr ✕) ]
-        return 0
-    }
-    rm -rf $target_dir/{.git*,README*.md,LICENSE}
-    current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
-    if ([[ -d $current_dir ]] && rm -rf $current_dir); then
-        mv -f $target_dir ${current_dir%/*}
-        print_info $(color cg 替换) $target_dir [ $(color cg ✔) ]
-    else
-        mv -f $target_dir $destination_dir
-        print_info $(color cb 添加) $target_dir [ $(color cb ✔) ]
-    fi
-}
-
-# 添加源仓库内的指定目录
-clone_dir() {
-    local repo_url branch temp_dir=$(mktemp -d)
-    if [[ "$1" == */* ]]; then
-        repo_url="$1"
-        shift
-    else
-        branch="-b $1 --single-branch"
-        repo_url="$2"
-        shift 2
-    fi
-    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
-        print_info $(color cr 拉取) $repo_url [ $(color cr ✕) ]
-        return 0
-    }
-    local target_dir source_dir current_dir
-    for target_dir in "$@"; do
-        source_dir=$(find_dir "$temp_dir" "$target_dir")
-        [[ -d $source_dir ]] || \
-        source_dir=$(find "$temp_dir" -maxdepth 4 -type d -name "$target_dir" -print -quit) && \
-        [[ -d $source_dir ]] || {
-            print_info $(color cr 查找) $target_dir [ $(color cr ✕) ]
-            continue
-        }
-        current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
-        if ([[ -d $current_dir ]] && rm -rf $current_dir); then
-            mv -f $source_dir ${current_dir%/*}
-            print_info $(color cg 替换) $target_dir [ $(color cg ✔) ]
-        else
-            mv -f $source_dir $destination_dir
-            print_info $(color cb 添加) $target_dir [ $(color cb ✔) ]
-        fi
-    done
-    rm -rf $temp_dir
-}
-
-# 添加源仓库内的所有目录
-clone_all() {
-    local repo_url branch temp_dir=$(mktemp -d)
-    if [[ "$1" == */* ]]; then
-        repo_url="$1"
-        shift
-    else
-        branch="-b $1 --single-branch"
-        repo_url="$2"
-        shift 2
-    fi
-    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
-        print_info $(color cr 拉取) $repo_url [ $(color cr ✕) ]
-        return 0
-    }
-    local target_dir source_dir current_dir
-    for target_dir in $(ls -l $temp_dir/$@ | awk '/^d/{print $NF}'); do
-        source_dir=$(find_dir "$temp_dir" "$target_dir")
-        current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
-        if ([[ -d $current_dir ]] && rm -rf $current_dir); then
-            mv -f $source_dir ${current_dir%/*}
-            print_info $(color cg 替换) $target_dir [ $(color cg ✔) ]
-        else
-            mv -f $source_dir $destination_dir
-            print_info $(color cb 添加) $target_dir [ $(color cb ✔) ]
-        fi
-    done
-    rm -rf $temp_dir
-}
-
-# 源仓库与分支
-SOURCE_REPO=$(basename $REPO_URL)
-echo "SOURCE_REPO=$SOURCE_REPO" >>$GITHUB_ENV
-
-# 平台架构
-TARGET_NAME=$(awk -F '"' '/CONFIG_TARGET_BOARD/{print $2}' .config)
-SUBTARGET_NAME=$(awk -F '"' '/CONFIG_TARGET_SUBTARGET/{print $2}' .config)
-DEVICE_TARGET=$TARGET_NAME-$SUBTARGET_NAME
-echo "DEVICE_TARGET=$DEVICE_TARGET" >>$GITHUB_ENV
-
-# Toolchain缓存文件名
-TOOLS_HASH=$(git log --pretty=tformat:"%h" -n1 tools toolchain)
-CACHE_NAME="$SOURCE_REPO-${REPO_BRANCH#*-}-$DEVICE_TARGET-cache-$TOOLS_HASH"
-echo "CACHE_NAME=$CACHE_NAME" >>$GITHUB_ENV
-
-# 下载并部署Toolchain
-if [[ $TOOLCHAIN = 'true' ]]; then
-    #cache_xa=$(curl -sL api.github.com/repos/$GITHUB_REPOSITORY/releases | awk -F '"' '/download_url/{print $4}' | grep $CACHE_NAME)
-    cache_xa="https://github.com/$GITHUB_REPOSITORY/releases/download/toolchain-cache/$CACHE_NAME.tzst"
-    cache_xc=$(curl -sL api.github.com/repos/Jejz168/toolchain-cache/releases | awk -F '"' '/download_url/{print $4}' | grep $CACHE_NAME)
-    #if [[ $cache_xa || $cache_xc ]]; then
-    if curl -Isf $cache_xa >/dev/null 2>&1 || [ $cache_xc ]; then
-        begin_time=$(date '+%H:%M:%S')
-        curl -Isf $cache_xa >/dev/null 2>&1 && wget -qc -t=3 $cache_xa || wget -qc -t=3 $cache_xc
-        [ -e *.tzst ]; status "下载toolchain缓存文件"
-        [ -e *.tzst ] && {
-            begin_time=$(date '+%H:%M:%S')
-            tar -I unzstd -xf *.tzst || tar -xf *.tzst
-           # [ $cache_xa ] || (cp *.tzst $GITHUB_WORKSPACE/output && echo "OUTPUT_RELEASE=true" >>$GITHUB_ENV)
-            sed -i 's/ $(tool.*\/stamp-compile)//' Makefile
-            [ -d staging_dir ]; status "部署toolchain编译缓存"
-        }
-    else
-        echo -e "$(color ch 下载toolchain缓存文件)                         [ $(color cr ✕) ]"
-        echo "CANCEL_TOOLCHAIN=true" >>$GITHUB_ENV
-    fi
-else
-    echo -e "$(color ch 使用toolchain缓存文件)                         [ $(color cr ✕) ]"
-    echo "CANCEL_TOOLCHAIN=true" >>$GITHUB_ENV
-fi
-
-# 创建插件保存目录
-destination_dir="package/A"
-[ -d $destination_dir ] || mkdir -p $destination_dir
-
-if [ -z "$DEVICE_TARGET" ] || [ "$DEVICE_TARGET" == "-" ]; then
-  echo -e "$(color cy 当前编译机型) $(color cb $SOURCE_REPO-${REPO_BRANCH#*-})"
-else
-  echo -e "$(color cy 当前编译机型) $(color cb $SOURCE_REPO-${REPO_BRANCH#*-}-$DEVICE_TARGET)"
-fi
