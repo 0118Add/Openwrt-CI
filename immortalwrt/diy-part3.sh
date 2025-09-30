ee20cd1cdd8fb1ba8eaac0997f36e9766a5b7a46
0118Add <60157610+0118Add@users.noreply.github.com>
2025-09-30 15:34:59 +0800

    Update diy-part3.sh

--- immortalwrt/diy-part3.sh
+++ immortalwrt/diy-part3.sh
@@ -185,12 +185,12 @@ curl -fsSL https://raw.githubusercontent.com/0118Add/Openwrt-CI/main/immortalwrt
 #popd
 
 # 报错修复
-if [ "$REPO_BRANCH" != "openwrt-23.05" ]; then
-    echo "开始修复报错……"
-	cp -f $GITHUB_WORKSPACE/patch/rust/* feeds/packages/lang/rust/Makefile
-    sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
-    echo "修复完成……"
-fi
+#if [ "$REPO_BRANCH" != "openwrt-23.05" ]; then
+#    echo "开始修复报错……"
+#	cp -f $GITHUB_WORKSPACE/patch/rust/* feeds/packages/lang/rust/Makefile
+#    sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
+#    echo "修复完成……"
+#fi
 
 # 拷贝自定义文件
 if [ -n "$(ls -A "${GITHUB_WORKSPACE}/immortalwrt/diy" 2>/dev/null)" ]; then
