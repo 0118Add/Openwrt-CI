50e5dc093fb934f39bb1d2a238afe76587b0aa46
0118Add <60157610+0118Add@users.noreply.github.com>
2026-03-27 18:33:52 +0800

    Update master.sh

--- x86/master.sh
+++ x86/master.sh
@@ -79,8 +79,8 @@ rm -rf feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # 预编译 node
-#rm -rf feeds/packages/lang/node/node
-#git clone --depth=1 -b packages-24.10 https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node/node
+rm -rf feeds/packages/lang/node/node
+git clone --depth=1 -b packages-24.10 https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node/node
 
 # luci-app-filemanager
 rm -rf feeds/luci/applications/luci-app-filemanager
