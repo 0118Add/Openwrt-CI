f9b1eb0e7613b7d57945d2008c836cf50fbfacf2
0118Add <60157610+0118Add@users.noreply.github.com>
2026-06-06 20:21:32 +0800

    Update diy-part4.sh

--- immortalwrt/diy-part4.sh
+++ immortalwrt/diy-part4.sh
@@ -59,14 +59,14 @@ sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" feeds/luci/applications/luci-app-ho
 #git clone https://github.com/nikkinikki-org/OpenWrt-momo  package/OpenWrt-momo
 
 # dae
-rm -rf feeds/luci/applications/{luci-app-dae,luci-app-daed}
-rm -rf feeds/packages/net/{dae,daed}
+#rm -rf feeds/luci/applications/{luci-app-dae,luci-app-daed}
+#rm -rf feeds/packages/net/{dae,daed}
 #git_sparse_clone kix https://github.com/QiuSimons/luci-app-dae luci-app-dae
 #git_sparse_clone master https://github.com/8688Add/openwrt_pkgs dae
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2026.04.30/g' package/dae/Makefile
 #sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=4f85d8f78958bea01a90da6feb315bcf766c4804/g' package/dae/Makefile
-git clone https://github.com/QiuSimons/luci-app-daed package/daed
-git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
+#git clone https://github.com/QiuSimons/luci-app-daed package/daed
+#git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
 
 # partexp
 git clone https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
