8e0550a995e27b3913f4909a923f783a1fc56044
0118Add <60157610+0118Add@users.noreply.github.com>
2026-06-08 19:39:33 +0800

    Update diy-part4.sh

--- immortalwrt/diy-part4.sh
+++ immortalwrt/diy-part4.sh
@@ -61,10 +61,9 @@ sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" feeds/luci/applications/luci-app-ho
 # dae
 #rm -rf feeds/luci/applications/{luci-app-dae,luci-app-daed}
 #rm -rf feeds/packages/net/{dae,daed}
-#git_sparse_clone kix https://github.com/QiuSimons/luci-app-dae luci-app-dae
-#git_sparse_clone master https://github.com/8688Add/openwrt_pkgs dae
-#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2026.04.30/g' package/dae/Makefile
-#sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=4f85d8f78958bea01a90da6feb315bcf766c4804/g' package/dae/Makefile
+#git_sparse_clone master https://github.com/QiuSimons/OpenWrt-Add luci-app-dae openwrt-einat-ebpf
+#sed -i 's/+@KERNEL_DEBUG_INFO_BTF/+vmlinux-btf/' package/openwrt-einat-ebpf/Makefile
+#git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
 
 # bpf-headers - 6.18
 #sed -ri "s/(PKG_PATCHVER:=)[^\"]*/\16.18/" package/kernel/bpf-headers/Makefile
